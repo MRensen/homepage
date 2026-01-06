1359322d4242b6c59c281ed95e326963912ca5c0
MRensen <markrensen@hotmail.com>
2026-01-06 21:34:56 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <header></header>
+      <h1>Mark Rensen</h1>
+        <p>Dit <a id="strikethrough">is</a> wordt mijn website</p>
+        <p>...under construction...</p>
+
+    </>
+  )
+}
+
+export default App
